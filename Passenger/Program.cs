9326f4af681bc5c310e281171d48526c6609ab65
Blast <kingprostyle@yandex.ru>
2025-03-15 06:24:13 +0300

    updated swagger gen

--- Passenger/Program.cs
+++ Passenger/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.OpenApi.Models;
 using Passenger.Models;
 using Passenger.Services;
 
@@ -36,7 +37,12 @@ public class Program
 
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         builder.Services.AddEndpointsApiExplorer();
-        builder.Services.AddSwaggerGen();
+        builder.Services.AddSwaggerGen(c =>
+        {
+            c.SwaggerDoc("v1", new OpenApiInfo { Title = "Passenger API", Version = "v1" });
+        });
+
+        builder.Services.AddSwaggerGenNewtonsoftSupport(); // Required for YAML support
 
         #region HTTP client registration
         builder.Services.AddHttpClient(
@@ -68,9 +74,17 @@ public class Program
         //if (app.Environment.IsDevelopment())
         //{
         app.UseDeveloperExceptionPage();
-        app.UseSwagger();
-        app.UseSwaggerUI();
-        //}
+        app.UseSwagger(c =>
+        {
+            c.RouteTemplate = "swagger/{documentName}/swagger.{json|yaml}";
+        });
+
+        // Enable middleware to serve swagger-ui 
+        app.UseSwaggerUI(c =>
+        {
+            c.SwaggerEndpoint("/swagger/v1/swagger.json", "Passenger module API V1 (JSON)");
+            c.SwaggerEndpoint("/swagger/v1/swagger.yaml", "Passenger module API V1 (YAML)");
+        });
 
         app.UseHttpsRedirection();
 
